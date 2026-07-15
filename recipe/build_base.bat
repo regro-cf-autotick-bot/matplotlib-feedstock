94851ed13031ec0b0d62cb41a7c610feebdd0452
Ryan May <rmay31@gmail.com>
2026-07-15 14:55:45 -0600

    Fix merge oops

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,24 +1,9 @@
 @echo on
 
-<<<<<<< HEAD
-set "MESON_ARGS=%MESON_ARGS% --buildtype=release --prefix=%LIBRARY_PREFIX% --pkg-config-path=%LIBRARY_LIB%\pkgconfig -Dlibdir=lib -Dsystem-freetype=false -Dsystem-qhull=true"
-
-if "%CI%" == "azure" (
-    :: Hack to try removing problematic Python from Azure CI image
-    :: Replace with conda-smithy solution when available
-    :: xref: https://github.com/conda-forge/conda-smithy/pull/1966
-    mkdir C:\empty
-    robocopy /purge /r:0 /w:0 /mt /ns /nc /np /nfl /ndl /njh /njs C:\empty C:\hostedtoolcache\windows\Python > nul 2>&1
-    rmdir /q C:\hostedtoolcache\windows\Python
-    rmdir /q C:\empty
-)
-
-=======
->>>>>>> main
 mkdir builddir
 
 %PYTHON% -m mesonbuild.mesonmain setup builddir %MESON_ARGS% ^
-    -Dsystem-freetype=true -Dsystem-libraqm=true -Dsystem-qhull=true
+    -Dsystem-freetype=false -Dsystem-libraqm=true -Dsystem-qhull=true
 if %ERRORLEVEL% NEQ 0 (type builddir\meson-logs\meson-log.txt && exit 1)
 
 %PYTHON% -m build --wheel ^
